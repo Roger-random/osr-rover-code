9e89b2353f4c2eeca5cf2b7119d9b0ebf6be1efc
Roger Cheng <roger.random@outlook.com>
2019-10-12 20:20:07 -0700

    Subscribe to ROS topic /joy

--- adafruit_servo/ServoNeckGamepad.py
+++ adafruit_servo/ServoNeckGamepad.py
@@ -13,6 +13,10 @@ http://wiki.ros.org/rosbridge_suite
 And the 'roslibpy' library is used here to make the Python code easier.
 https://roslibpy.readthedocs.io
 """
+# Standard Python libraries
+import time
+
+# Python library for connecting to ROS via ROS bridge suite WebSocket server
 import roslibpy
 
 # IP address or name of machine running ROS master
@@ -38,6 +42,9 @@ class ServoNeckGamepad:
   react accordingly
   """
   def __init__(self):
+    """
+    Class constructor sets up an instance of roslibpy to use
+    """
     progress("Creating client for {0}:{1}".format(ROS_MASTER, ROS_BRIDGE_PORT))
     self.ros_client = roslibpy.Ros(host=ROS_MASTER, port=ROS_BRIDGE_PORT)
 
@@ -45,10 +52,37 @@ class ServoNeckGamepad:
     """
     Main entry point of ServoNeckGamepad class
     """
-    progress("Connecting to ROS master")
-    self.ros_client.run(ROS_CONNECT_TIMEOUT)
-    progress("Connection status: {0}".format(self.ros_client.is_connected))
-    self.ros_client.terminate()
+    try:
+      progress("Connecting to ROS master")
+      # self.ros_client.run_forever makes more sense, but it doesn't have a
+      # timeout parameter
+      self.ros_client.run(ROS_CONNECT_TIMEOUT)
+
+      # When we're connected and ready, get to work.
+      self.ros_client.on_ready(self.on_ready_callback)
+
+      # Since we can't use run_forever(), we end up with run(TIMEOUT)
+      # and check connected status every 5 seconds
+      while self.ros_client.is_connected:
+        time.sleep(5.0)
+    finally:
+      progress("Terminating ROS client")
+      self.ros_client.terminate()
+
+  def on_ready_callback(self):
+    """
+    When ROS connection is ready, subscribe to /joy topic
+    """
+    topicJoy = roslibpy.Topic(self.ros_client, '/joy', 'sensor_msgs/Joy')
+    topicJoy.subscribe(self.joystick_callback)
+
+  def joystick_callback(self, message):
+    """
+    When a /joy topic message arrives, examine buttons state given in
+    message['buttons'] to determine action
+    Reference: http://docs.ros.org/kinetic/api/sensor_msgs/html/msg/Joy.html
+    """
+    progress("/joy buttons: {1} @ {0}".format(time.time(),message['buttons']))
 
 if __name__ == "__main__":
   sng = ServoNeckGamepad()
